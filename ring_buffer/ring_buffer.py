51794fd4bce76496000abc8cf2c55c3604162b8b
stevedole808 <stevedolelambda@gmail.com>
2020-07-17 13:53:33 -1000

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.size = 0
+
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.storage:
+            self.storage.append(item)
+            self.size += 1
+        else:
+            self.storage[self.size] = item
+            self.size = ((self.size + 1) % self.capacity)
 
     def get(self):
-        pass
+        storage = []
+        for x in self.storage:
+            if x is not None:
+                storage.append(x)
+        return storage
